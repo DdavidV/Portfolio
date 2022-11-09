3c5f83f449c05d76950b786629c116cff7603a5e
Duneczki Dávid Vendel <themani@hotmail.hu>
2022-11-09 10:37:00 +0100

    first commit

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default[
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Room.glb"
+    },
+    {
+        name: "screen",
+        type: "imageTexture",
+        path: "/textures/mountains.jpg"
+    }
+]
