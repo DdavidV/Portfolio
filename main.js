2fa924bce5ab38b178ff3397f858bc19e2c10e44
Duneczki Dávid Vendel <themani@hotmail.hu>
2022-11-09 15:15:00 +0100

    main page design

--- main.js
+++ main.js
@@ -2,3 +2,30 @@ import './style.css'
 import Experience from './Experience/Experience'
 
 const experience = new Experience(document.querySelector(".experience-canvas"))
+
+const myTags = [
+    'JavaScript', 'CSS', 'HTML',
+    'C#', 'Ms Office', 'Python',
+    'Sql', 'Git', 'Three.js',
+    'Java', 'Firebase', 'Android Studio',
+];
+
+var tagCloud = TagCloud('.sphere', myTags,{
+
+    // radius in px
+    radius: 150,
+  
+    // animation speed
+    // slow, normal, fast
+    maxSpeed: 'normal',
+    initSpeed: 'normal',
+  
+    // 0 = top
+    // 90 = left
+    // 135 = right-bottom
+    direction: 135,
+  
+    // interact with cursor move on mouse out
+    keep: true
+  
+  }); 
